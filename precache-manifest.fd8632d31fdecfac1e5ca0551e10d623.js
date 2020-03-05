6d83b438735aba2a907725b4c252c6b72ba44071
sskullgra <sskullgra@gmail.com>
2020-03-05 19:30:27 +0800

    Updates

---
+++ precache-manifest.fd8632d31fdecfac1e5ca0551e10d623.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef84b117e6cac405635e54f3fdc4d5dc",
+    "url": "/hello/index.html"
+  },
+  {
+    "revision": "cea38ed25e11bc1b7a30",
+    "url": "/hello/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1d6d513f8e171b8b3317",
+    "url": "/hello/static/js/2.5026ad79.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/hello/static/js/2.5026ad79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cea38ed25e11bc1b7a30",
+    "url": "/hello/static/js/main.1baf54e0.chunk.js"
+  },
+  {
+    "revision": "2b3832525503dbf599d8",
+    "url": "/hello/static/js/runtime-main.de1b96e8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hello/static/media/logo.5d5d9eef.svg"
+  }
+]);
